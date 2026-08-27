9fa6eefea4544f3aba4ccf3c35426942b60072b9
Joe <33161944+jOethebuilder@users.noreply.github.com>
2026-08-27 04:33:44 -0700

    Delete K9Mobile(1)/K9Mobile directory

--- K9Mobile(1)/K9Mobile/app/src/main/java/com/joethebuilder/k9/viewmodel/SubMenuTagState.kt
+++
@@ -1,4 +0,0 @@
-package com.joethebuilder.k9.viewmodel
-
-/** Mirrors firmware's TagStatus subset used by sub-menu screens (TAG_NONE/TAG_BLANK/has-data). */
-enum class SubMenuTagState { NONE, BLANK, PRESENT }
