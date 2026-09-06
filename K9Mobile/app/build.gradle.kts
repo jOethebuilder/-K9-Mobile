f64404db36804eb9619cf85c16a8e80888c9e426
Joe <33161944+jOethebuilder@users.noreply.github.com>
2026-09-05 18:28:14 -0700

    Update build.gradle.kts

--- K9Mobile/app/build.gradle.kts
+++ K9Mobile/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
     // Networking (Moonraker HTTP)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     // Persisted settings (replaces ESP32 Preferences/NVS)
